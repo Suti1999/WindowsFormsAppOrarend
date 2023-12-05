e5ea6d47286e0f07c67d00c1305151c2da47cb98
ozsva <ozsva@DESKTOP-2VK0BVF>
2023-12-05 22:41:23 +0100

    Add project files.

---
+++ WindowsFormsAppOrarend/Database.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsAppOrarend
+{
+    internal class Database
+    {
+      
+        MySqlConnection connection;
+        MySqlCommand command;
+
+      
+        public Database()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "orarend";
+            builder.CharacterSet = "utf8";
+            connection = new MySqlConnection(builder.ConnectionString);
+            command = connection.CreateCommand();
+
+            try
+            {
+               
+                kapcsolatNyitas();
+
+                kapcsolatZarasa();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+
+        
+        public void kapcsolatZarasa()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        /
+        public void kapcsolatNyitas()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public int Bejelentkezes(string nev, string jelszo)
+        {
+            int userid = -1;
+            kapcsolatNyitas();
+            command.CommandText = "SELECT tanarok.nev, tanarok.jelszo FROM orarend WHERE tanarok.nev=@nev";
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@nev", nev);
+            MySqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                string taroltJelszo = reader.GetString("jelszo");
+                if (taroltJelszo.Equals(jelszo))
+                {
+                    userid = reader.GetInt32("tanarid");
+                }
+            }
+            kapcsolatZarasa();
+            return userid;
+        }
+    }
+}
---
+++ WindowsFormsAppOrarend/FormLogin.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsAppOrarend
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_loginnev = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_loginjelszo = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Menu;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(102, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BEJELENTKEZÉS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Név";
+            // 
+            // textBox_loginnev
+            // 
+            this.textBox_loginnev.Location = new System.Drawing.Point(105, 106);
+            this.textBox_loginnev.Name = "textBox_loginnev";
+            this.textBox_loginnev.Size = new System.Drawing.Size(200, 20);
+            this.textBox_loginnev.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Jelszó";
+            // 
+            // textBox_loginjelszo
+            // 
+            this.textBox_loginjelszo.Location = new System.Drawing.Point(105, 169);
+            this.textBox_loginjelszo.Name = "textBox_loginjelszo";
+            this.textBox_loginjelszo.Size = new System.Drawing.Size(200, 20);
+            this.textBox_loginjelszo.TabIndex = 4;
+            // 
+            // button_login
+            // 
+            this.button_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_login.Location = new System.Drawing.Point(105, 195);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(200, 36);
+            this.button_login.TabIndex = 5;
+            this.button_login.Text = "BEJELENTKEZÉS";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(392, 248);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.textBox_loginjelszo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_loginnev);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormLogin";
+            this.Text = "Bejelentkezés";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_loginnev;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_loginjelszo;
+        private System.Windows.Forms.Button button_login;
+    }
+}
+
